6feb8163ccb270ca74c59812f438f3cd33328cec
unknown <dan.nadlerp@gmail.com>
2024-02-29 10:22:54 -0500

    added env to ignore, removed routes files, updated DB connection code to index.js, changed import export between connection and index

--- config/connection.js
+++ config/connection.js
@@ -8,4 +8,22 @@ const connection = mysql.createConnection({
   password: DB_PWD,
   database: "employee_tracker_db",
 });
-module.exports = connection;
+connection.connect((err) => {
+  if (err) {
+    console.log(`Error: ${err}`);
+    return;
+  }
+  console.log("Connected to SQL server");
+});
+
+// Middleware to handle errors
+function errorHandler(res, error) {
+  console.error(error);
+  res.status(500).json({ error: "Internal server error" });
+}
+
+router.get("/", (req, res) => {
+  connection.query("select all from employee");
+});
+
+(module.exports = mysql), connection, errorHandler;
--- index.js
+++ index.js
@@ -1,11 +1,7 @@
-const inquirer = require('inquirer');
-const mysql2 = require('mysql2');
-const express = require('express');
-const app = express();
-const employeeRoutes = require('./routes/employeeRoutes');
-const roleRoutes = require('./routes/roleRoutes');
-const deptRoutes = require('./routes/deptRoutes');
+import { mysql, connection, errorHandler } from "./config/connection";
+const inquirer = require("inquirer");
 
+/* const app = express();
 app.use('/employee', employeeRoutes);
 app.use('/role', roleRoutes);
 app.use('/dept', deptRoutes);
@@ -14,26 +10,48 @@ const port = process.env.PORT || 3001;
 app.listen(port, () => {
   console.log("listening...");
 });
-
+ */
+//connects to DB
+connection.connect((err) => {
+  if (err) throw err;
+  console.log("Database connected");
+  init();
+});
+//middleware
+errorHandler();
 
 const init = async () => {
-mysql2
-await inquirer
+  mysql;
+  await inquirer
 
-.prompt([
-  {
-    name: "menu",
-    type: "list",
-    choices: ["View all departments", "View all roles", "View all employees", "Add a department", "Add a role", "Add an employee", "Update an employee role"],
-    message: "Main Menu: Please make a selection.",
-  }
-  
-])
+    .prompt([
+      {
+        name: "main_selection",
+        type: "list",
+        choices: [
+          "View all departments",
+          "View all roles",
+          "View all employees",
+          "Add a department",
+          "Add a role",
+          "Add an employee",
+          "Update an employee role",
+        ],
+        message: "Main Menu: Please make a selection.",
+      },
+    ])
 
-.then(
-    // => { };
-)
+    .then((answers) => {
+      if (answers.prompt === "View all departments") {
+        connection.query("SELECT * FROM department", (err, res) => {
+          if (err) throw err;
+          console.log("View all departments:");
+          console.table(res);
+          init();
+        });
+      }
+    });
 
-console.log("finished");
-}
+  console.log("finished");
+};
 init();
