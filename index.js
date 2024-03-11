44a0860bb54893a48354a98696acdc18c724cb61
Dan Nadler <dan.nadlerp@gmail.com>
2024-03-11 12:07:10 -0400

    added functionality to add employee function. refactoring to fix errors

--- index.js
+++ index.js
@@ -121,6 +121,66 @@ function init() {
               );
             });
         });
+      } else if (answers.main_selection === "Add an employee") {
+        connection.query("SELECT * FROM role", (err, roleRes) => {
+          if (err) throw err;
+          const roleChoices = roleRes.map((role) => role.title);
+
+          connection.query("SELECT * FROM employee", (err, employeeRes) => {
+            console.log("employee rec:", employeeRes);
+            if (err) throw err;
+            const employeeChoices = employeeRes.map((employee) => [
+              employee.first_name_input,
+              employee.last_name_input,
+            ]);
+
+            inquirer
+              .prompt([
+                {
+                  name: "first_name_input",
+                  type: "input",
+                  message: "Please input the employee's first name:",
+                },
+                {
+                  name: "last_name_input",
+                  type: "input",
+                  message: "Please input the employee's last name:",
+                },
+                {
+                  name: "role_select",
+                  type: "list",
+                  choices: roleChoices,
+                  message: "Please select which department the role is in:",
+                },
+              ])
+              .then((answer) => {
+                const roleSelectID = roleChoices.indexOf(answer.role_select);
+                console.log(`role ID: ${roleSelectID}`);
+                /*
+                const roleDeptID = departmentChoices.indexOf(answer.role_dept);
+
+                const roleSelectID = roleRes.map((role) => ({
+                  name: role.title,
+                  value: role.id,
+                }));
+ */
+                connection.query(
+                  "INSERT INTO employee (first_name, last_name, role_id) VALUES (?, ?, ?)", //VALUES (?) creates a placeholder for future value
+                  [
+                    answer.first_name_input,
+                    answer.last_name_input,
+                    roleSelectID,
+                  ],
+                  (err, res) => {
+                    if (err) throw err;
+                    console.log("employee added successfully");
+
+                    init(); //brings back to menu
+                  }
+                );
+              });
+          });
+        });
       }
       /* .catch((error) => {
       errorHandler(res, err); */
